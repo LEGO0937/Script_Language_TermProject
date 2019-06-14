acd27b0e9635a263029cc6a300efe249c27b59b6
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2019-06-14 20:55:22 +0900

    C++연동 수정
    URL생성을 더욱 편하게 하기위해 수정하였다

--- 스크립트언어기획/spam/spam/spammodule.c
+++ 스크립트언어기획/spam/spam/spammodule.c
@@ -3,26 +3,27 @@
 static PyObject *
 make_url(PyObject *self, PyObject *args)// base_url, 서비스키, 시도, 군구, 자세한 항목
 {
-	char* str_main;
-	char* str1;
-	char* str2;
-	char* str3;
-	char* str4;
+	char* str_main = "http://openapi.tour.go.kr/openapi/service/TourismResourceService/getTourResourceList?ServiceKey=qmAs0ut6m%2BwM%2FJwamfdK8RkKJz5yNmI4VrT6DEUuwmm%2FW7GMClJBCltEmgQEeSo7v1poVh0ZYPSbihUbMftNUQ%3D%3D&SIDO=";
+	char* str1 = "&GUNGU=";
+	char* str2 = "&RES_NM=";
+	char* str_sido;
+	char* str_gugun;
+	char* str_name;
 
 	//int len;
-	if (!PyArg_ParseTuple(args, "sssss", &str_main, &str1, &str2, &str3, &str4))
+	if (!PyArg_ParseTuple(args, "sss", &str_sido, &str_gugun, &str_name))
 		return NULL;
 
 
 
+	strcat(str_main, str_sido);
 	strcat(str_main, str1);
+	strcat(str_main, str_gugun);
 	strcat(str_main, str2);
-	strcat(str_main, str3);
-	strcat(str_main, str4);
+	strcat(str_main, str_name);
 
 
 
-	//len = strlen(str);
 	return Py_BuildValue("s", str_main);
 }
 
@@ -47,3 +48,4 @@ PyInit_spam(void)
 
 
 //C:\Users\jung9\AppData\Local\Programs\Python\Python37\include
+// http://openapi.tour.go.kr/openapi/service/TourismResourceService/getTourResourceList?ServiceKey=qmAs0ut6m%2BwM%2FJwamfdK8RkKJz5yNmI4VrT6DEUuwmm%2FW7GMClJBCltEmgQEeSo7v1poVh0ZYPSbihUbMftNUQ%3D%3D&SIDO=서울특별시&GUNGU=종로구&RES_NM=경복궁
